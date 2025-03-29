457908ed735162ae97bdc8b485e463d12b598d84
Eric Hernandez <ehlabrada@gmail.com>
2025-03-29 13:49:08 -0400

    Modify the Auth file, for ensuring Window context properly

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -3,14 +3,24 @@
 import { Auth } from "@supabase/auth-ui-react"
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 import { supabaseClient } from "../../../lib/supabaseClient"
-
+import { useEffect, useState } from "react"
 
 export default function AuthUI() {
+    const [isBrowser, setIsBrowser] = useState(false)
+
+    useEffect(() => {
+        setIsBrowser(true)
+    }, [])
+
     return (
-        <Auth
-            supabaseClient={supabaseClient}
-            appearance={{ theme: ThemeSupa }}
-            redirectTo={`${window.location.origin}/auth/callback`}
-        />
+        <>
+            {isBrowser && (
+                <Auth
+                    supabaseClient={supabaseClient}
+                    appearance={{ theme: ThemeSupa }}
+                    redirectTo={`${window.location.origin}/auth/callback`}
+                />
+            )}
+        </>
     )
 }
